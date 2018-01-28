284d6d68f4d7844e680cdd5061d8389d126441a8
Adrian Saycon <adzbite@gmail.com>
2018-01-28 08:00:55 +0800

    acceptance

---
+++ environments/acceptance/common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=test_gogetadz',
+            'username' => 'test_gogetadz',
+            'password' => 'Qwerasdf!234',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
