8802ffe2e1f0bf478190eed50fe6286e991dd84a
Adrian Saycon <adzbite@gmail.com>
2018-01-31 01:57:37 +0800

    ...

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=test_gogetadz',
-            'username' => 'test_gogetadz',
-            'password' => 'Qwerasdf!234',
+            'dsn' => 'mysql:host=localhost;dbname=admin_mygogetadz',
+            'username' => 'admin_mygogetadz',
+            'password' => 'KPvn7JeHnw',
             'charset' => 'utf8',
         ],
         'mailer' => [
