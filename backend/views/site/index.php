502ddd20798c2cd617f95124cef4966c27e5263b
paposaycon <paposaycon@gmail.com>
2018-02-20 14:13:08 +0800

    home earnings

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -6,34 +6,43 @@ $this->title = 'Home';
 ?>
 <style>
 .block1 {
-    position: absolute;	
-	width:100%;
-	
+    position:relative;;
+	width:120%;
+	margin-top:0%;
 }
 .block2 {
-    position: absolute;	
-	top:35%;
-	width:100%;
+    position:relative;
+	//margin-top:22%;
+	width:120%;
 }
 .block3 {
-    position: absolute;	
-	top:50%;
-	width:100%;
+    position:relative;
+	margin-top:0%;
+	width:120%;
 }
 .block4 {
-    position: absolute;	
-	top:65%;
-	width:100%;
+    position:relative;
+	//margin-top:40%;
+	width:120%;
+}
+.container{
+	//width:1000px;
+	position:relative;
+	top:%;
+	left:-3%;
+    //overflow: scroll;
 }
 </style>
-<div class="block1">
+<div class="container">
+<h4 style="margin-top:2px;margin-left:4%">Ads clicked Details</h1>
+<div class="block1" >
 <section>  
   <div class="col-md-3 col-sm-6">
     <div class="info-box">
       <span class="info-box-icon bg-blue"><i class="fa fa-list-ol"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Ads Clicked</span>
-        <span class="info-box-number"><?= $clickCount ?></span>
+        <span class="info-box-number" style="text-align:center;line-height:40px"><?= $clickCount ?></span>
       </div>
     </div>
   </div>
@@ -43,7 +52,7 @@ $this->title = 'Home';
       <span class="info-box-icon bg-orange"><i class="fa fa-usd"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Earnings</span>
-        <span class="info-box-number"><?= Yii::$app->appConfig->getCurrencySymbol() ?> <?= $totalIncome ?></span>
+        <span class="info-box-number" style="text-align:center;line-height:40px"><?= Yii::$app->appConfig->getCurrencySymbol() ?> <?= $totalIncome ?></span>
         <!--<a href="<?= \yii\helpers\Url::to(['/withdrawal/request']) ?>" class="btn btn-sm btn-default btn-block">Widthraw</a>-->
       </div>
     </div>
@@ -52,8 +61,8 @@ $this->title = 'Home';
     <div class="info-box">
       <span class="info-box-icon bg-green"><i class="fa fa-usd"></i></span>
       <div class="info-box-content">
-        <span class="info-box-text">Withdraw</span>
-        <span class="info-box-number"><?= Yii::$app->appConfig->getCurrencySymbol() ?> <?= $totalIncome ?></span>
+        <span class="info-box-text">Total clicked Earnings</span>
+        <span class="info-box-number"style="text-align:center;line-height:30px"><?= Yii::$app->appConfig->getCurrencySymbol() ?> <?= $totalIncome ?></span>
         <a href="<?= \yii\helpers\Url::to(['/withdrawal/request']) ?>" class="btn btn-sm btn-default btn-block">Widthraw</a>
       </div>
     </div>
@@ -61,14 +70,15 @@ $this->title = 'Home';
 </section>
 </div>
 
-<div class="block2">
+<h4 style="margin-top:12%;margin-left:4%">Binary Statement</h1>
+<div class="block2" style="float:left">
 <section>  
   <div class="col-md-3 col-sm-6">
     <div class="info-box">
       <span class="info-box-icon bg-blue"><i class="fa fa-list-ol"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Sign-up Bonus</span>
-        <span class="info-box-number"></span>
+        <span class="info-box-number"style="text-align:center;line-height:40px"></span>
       </div>
     </div>
   </div>
@@ -78,7 +88,7 @@ $this->title = 'Home';
       <span class="info-box-icon bg-orange"><i class="fa fa-usd"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Earnings</span>
-        <span class="info-box-number"><?= Yii::$app->appConfig->getCurrencySymbol() ?></span>
+        <span class="info-box-number"style="text-align:center;line-height:40px"><?= Yii::$app->appConfig->getCurrencySymbol() ?></span>
         <!--<a href="<?= \yii\helpers\Url::to(['/withdrawal/request']) ?>" class="btn btn-sm btn-default btn-block">Widthraw</a>-->
       </div>
     </div>
@@ -86,14 +96,14 @@ $this->title = 'Home';
    
 </section>
 </div>
-<div class="block3">
+<div class="block3"style="float:left">
 <section>  
   <div class="col-md-3 col-sm-6">
     <div class="info-box">
       <span class="info-box-icon bg-blue"><i class="fa fa-list-ol"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Direct referrals</span>
-        <span class="info-box-number"></span>
+        <span class="info-box-number"style="text-align:center;line-height:40px"></span>
       </div>
     </div>
   </div>
@@ -103,7 +113,7 @@ $this->title = 'Home';
       <span class="info-box-icon bg-orange"><i class="fa fa-usd"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Earnings</span>
-        <span class="info-box-number"><?= Yii::$app->appConfig->getCurrencySymbol() ?> </span>
+        <span class="info-box-number"style="text-align:center;line-height:40px"><?= Yii::$app->appConfig->getCurrencySymbol() ?> </span>
         <!--<a href="<?= \yii\helpers\Url::to(['/withdrawal/request']) ?>" class="btn btn-sm btn-default btn-block">Widthraw</a>-->
       </div>
     </div>
@@ -112,13 +122,13 @@ $this->title = 'Home';
 </section>
 </div>
  
-<div class="block4"> 
+<div class="block4"style="float:left"> 
  <section> <div class="col-md-3 col-sm-6">
     <div class="info-box">
       <span class="info-box-icon bg-blue"><i class="fa fa-list-ol"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Pairing Bonus</span>
-        <span class="info-box-number"></span>
+        <span class="info-box-number" style="text-align:center;line-height:40px"></span>
       </div>
     </div>
   </div>
@@ -128,7 +138,7 @@ $this->title = 'Home';
       <span class="info-box-icon bg-orange"><i class="fa fa-usd"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Earnings</span>
-        <span class="info-box-number"><?= Yii::$app->appConfig->getCurrencySymbol() ?></span>
+        <span class="info-box-number"style="text-align:center;line-height:40px"><?= Yii::$app->appConfig->getCurrencySymbol() ?></span>
         <!--<a href="<?= \yii\helpers\Url::to(['/withdrawal/request']) ?>" class="btn btn-sm btn-default btn-block">Widthraw</a>-->
       </div>
     </div>
@@ -139,10 +149,11 @@ $this->title = 'Home';
       <span class="info-box-icon bg-green"><i class="fa fa-usd"></i></span>
       <div class="info-box-content">
         <span class="info-box-text">Total Binary Earnings</span>
-        <span class="info-box-number"><?= Yii::$app->appConfig->getCurrencySymbol() ?> </span>
+        <span class="info-box-number" style="text-align:center;line-height:30px"><?= Yii::$app->appConfig->getCurrencySymbol() ?> </span>
         <a href="<?= \yii\helpers\Url::to(['/withdrawal/request']) ?>" class="btn btn-sm btn-default btn-block">Widthraw</a>
       </div>
     </div>
   </div>
 </section>
+</div>
 </div>
