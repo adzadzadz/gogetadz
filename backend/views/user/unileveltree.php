f6820c263af652647118a3b2a579d740894470ca
Adrian Saycon <adzbite@gmail.com>
2018-05-25 17:49:04 +0800

    unilevel

--- backend/views/user/unileveltree.php
+++ backend/views/user/unileveltree.php
@@ -8,43 +8,46 @@ $this->title = 'Unilevel Tree';
 
 <style>
 .user-data {
+  background: #bab;
+  display: inline-block;
+}
+.item {
   width: 70px;
-  height: 70px;
+  min-height: 70px;
   background: #faf;
   margin: 4px; 
-  float: left;
-  position: relative;
 }
-
 .branch {
   display: inline-block;
+  background: #aba;
 }
 </style>
 
 <div class="user-data lvl0">
-  <0: <?= $id ?>>
-</div>
-<?php foreach ($user->network->downlines as $lvl1) { ?>
-<div class="branch">
-  <div class="user-data lvl1">
-    <1: <?= $lvl1->id ?>>
+  <div class="item">
+    <0: <?= $id ?>>
   </div>
-  <br>
-  <?php foreach ($lvl1->network->downlines as $lvl2) { ?>
 
-  <div class="user-data lvl2">
-    <2: <?= $lvl1->id ?>>
-  </div>
-  <br>
-  <?php foreach ($lvl2->network->downlines as $lvl3) { ?>
+  <?php foreach ($user->network->downlines as $lvl1) { ?>
+    <div class="user-data lvl1">
+      <div class="item">
+        <<?= $id ?>: <?= $lvl1->id ?>>
+      </div>
+    
 
-  <div class="user-data lvl3">
-    <3: <?= $lvl1->id ?>>
-  </div>
-</div>
+      <?php foreach ($lvl1->network->downlines as $lvl2) { ?>
+
+      <div class="user-data lvl2">
+        <div class="item">
+          <<?= $lvl1->id ?>: <?= $lvl2->id ?>>
+        </div>
+      </div>
+    </div>
 <!-- Foreach Closing Tags -->
 <?php
-        }
+        
     }
 }
 ?>
+
+</div>
